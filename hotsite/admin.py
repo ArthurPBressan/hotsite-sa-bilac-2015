8a3796faee958d079ac28da034ffa105f44829b9
Arthur Bressan <arthurpbressan@gmail.com>
2015-10-20 23:40:02 -0200

    Aidiciona flask admin

--- hotsite/admin.py
+++ hotsite/admin.py
@@ -1,6 +1,49 @@
 # coding: UTF-8
 from __future__ import absolute_import
 
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
+
+from hotsite.base import db
+from hotsite.models import Aluno, Palestra, PalestraAluno
+
+
+class _ModelView(ModelView):
+
+    category = None
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('name', self.name)
+        super(_ModelView, self).__init__(self.model, db.session, *args, **kwargs)
+
+
+class AlunoView(_ModelView):
+
+    form_excluded_columns =  ['palestras']
+
+    model = Aluno
+    name = 'Aluno'
+
+
+class PalestraView(_ModelView):
+    form_excluded_columns = ['alunos']
+
+    model = Palestra
+    name = 'Palestra'
+
+
+class PalestraAlunoView(_ModelView):
+    model = PalestraAluno
+    name = 'Palestra x Aluno'
+
 
 def init_app(app):
+    views = [
+        AlunoView(),
+        PalestraView(),
+        PalestraAlunoView(),
+    ]
+    admin = Admin(app)
+    for view in views:
+        admin.add_view(view)
     pass
