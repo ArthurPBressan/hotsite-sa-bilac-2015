e69ef748b85a7a6a816b14e98f20a0f2e5d8f6b2
Arthur Bressan <arthurpbressan@gmail.com>
2015-10-27 21:56:25 -0200

    Adiciona rota de comentário

--- hotsite/rotas.py
+++ hotsite/rotas.py
@@ -1,7 +1,7 @@
 # coding: UTF-8
 from __future__ import absolute_import
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 from hotsite.models import Palestra
 
@@ -22,3 +22,12 @@ def index():
         palestras_trilha.append(palestra)
     trilhas = set(palestra.trilha for palestra in palestras_q)
     return render_template('index.html', palestras=palestras, trilhas=trilhas)
+
+
+@bp.route('/<palestra>', methods=['GET', 'POST'])
+def rate_palestra(palestra):
+    palestra = Palestra.query.get_or_404(palestra)
+    if request.method == 'POST':
+        print request.form
+        return redirect(url_for('.index'))
+    return render_template('rate_palestra.html', palestra=palestra)
