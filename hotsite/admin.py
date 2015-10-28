f72f656d06f2fbe51424cc973c75881c0c18275e
Arthur Bressan <arthurpbressan@gmail.com>
2015-10-27 23:47:58 -0200

    Protege o admin

--- hotsite/admin.py
+++ hotsite/admin.py
@@ -1,11 +1,21 @@
 # coding: UTF-8
 from __future__ import absolute_import
 
+from flask import current_app
 from flask.ext.admin import Admin
+from flask.ext.admin import BaseView
 from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.login import current_user
 
 from hotsite.base import db
 from hotsite.models import Aluno, Palestra, PalestraAluno
+
+
+def handle_view(self, name, **kwargs):
+    if not current_user.is_authenticated or current_user.ra != 'admin':
+        return current_app.login_manager.unauthorized()
+
+BaseView._handle_view = handle_view
 
 
 class _ModelView(ModelView):
